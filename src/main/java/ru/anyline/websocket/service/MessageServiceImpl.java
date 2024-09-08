efd9c608980125393fabe0241f84a2b8d69ca9f7
Anyline <s09857404@gmail.com>
2024-09-08 20:41:09 +0300

    h2 service

--- src/main/java/ru/anyline/websocket/service/MessageServiceImpl.java
+++ src/main/java/ru/anyline/websocket/service/MessageServiceImpl.java
@@ -1,17 +1,30 @@
 package ru.anyline.websocket.service;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.anyline.websocket.entity.Message;
+import ru.anyline.websocket.repository.MessageRepo;
 
 import java.util.List;
 
+@AllArgsConstructor
+@Service
 public class MessageServiceImpl implements MessageService{
+
+    private final MessageRepo messageRepo;
     @Override
     public void saveMessage(String roomId, String content) {
         
+        Message message = new Message();
+        message.setRoomId(roomId);
+        message.setContent(content);
+        messageRepo.save(message);
+
     }
 
     @Override
     public List<Message> getMessageHistory(String roomId) {
-        return null;
+
+        return messageRepo.findByRoomId(roomId);
     }
 }
