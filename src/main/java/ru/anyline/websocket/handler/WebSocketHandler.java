4b037275c82eede8f6b32ab082367aaedf8afff5
Anyline <s09857404@gmail.com>
2024-09-08 20:50:43 +0300

    handler +

--- src/main/java/ru/anyline/websocket/handler/WebSocketHandler.java
+++ src/main/java/ru/anyline/websocket/handler/WebSocketHandler.java
@@ -1,20 +1,28 @@
 package ru.anyline.websocket.handler;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import ru.anyline.websocket.service.MessageServiceImpl;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 @AllArgsConstructor
+@NoArgsConstructor
 public class WebSocketHandler extends TextWebSocketHandler {
 
-    private final MessageServiceImpl messageService;
+    private MessageServiceImpl messageService;
+
     private static Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<>());
 
     @Override
@@ -51,6 +59,30 @@ public class WebSocketHandler extends TextWebSocketHandler {
             session.close(CloseStatus.SERVER_ERROR);
         }
     }
+
+    @Override
+    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
+        byte[] payload = message.getPayload().array();
+        System.out.println("Received binary message of size: " + payload.length);
+
+        Path path = Paths.get("uploads/file_" + session.getId());
+        try {
+            Files.write(path, payload);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen()) {
+                try {
+                    webSocketSession.sendMessage(new TextMessage("Binary message received from session " + session.getId()));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
 
 }
 
