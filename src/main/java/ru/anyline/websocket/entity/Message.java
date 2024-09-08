2ca61426bc8e54ad317929435104c2d633b2a51e
Anyline <s09857404@gmail.com>
2024-09-08 20:32:48 +0300

    h2

---
+++ src/main/java/ru/anyline/websocket/entity/Message.java
@@ -0,0 +1,20 @@
+package ru.anyline.websocket.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roomId;
+    private String content;
+
+
+}
