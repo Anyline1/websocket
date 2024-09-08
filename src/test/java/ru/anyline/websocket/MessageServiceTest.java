35a8f81febd1c6019bf7ca195bf5ce5961bee910
Anyline <s09857404@gmail.com>
2024-09-08 21:12:44 +0300

    test+

--- src/test/java/ru/anyline/websocket/MessageServiceTest.java
+++ src/test/java/ru/anyline/websocket/MessageServiceTest.java
@@ -5,10 +5,17 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.web.socket.BinaryMessage;
+import org.springframework.web.socket.TextMessage;
 import ru.anyline.websocket.entity.Message;
 import ru.anyline.websocket.repository.MessageRepo;
 import ru.anyline.websocket.service.MessageService;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class MessageServiceTest {
@@ -42,5 +49,20 @@ public class MessageServiceTest {
 
         verify(messageRepository, times(1)).findByRoomId(roomId);
     }
+
+//    @Test
+//    public void testHandleBinaryMessage() throws Exception {
+//        byte[] payload = "Test Binary Data".getBytes();
+//        BinaryMessage binaryMessage = new BinaryMessage(payload);
+//
+//        when(webSocketSession.isOpen()).thenReturn(true);
+//
+//        webSocketHandler.handleBinaryMessage(webSocketSession, binaryMessage);
+//
+//        Path path = Paths.get("uploads/file_" + webSocketSession.getId());
+//        assertTrue(Files.exists(path));
+//
+//        verify(webSocketSession, times(1)).sendMessage(any(TextMessage.class));
+//    }
 }
 
